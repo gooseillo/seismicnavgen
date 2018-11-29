e56d1aabb9a9eb7d41149980769a0b5e0392ba77
teryne <teryne@gmail.com>
2018-11-29 12:42:06 +0800

    req.user

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', secured(), function(req, res, next) {
-  res.render('index');
+  res.render('index', { user: req.user });
 });
 
 router.get('/landing', function(req, res, next){
